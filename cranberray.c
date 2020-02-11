9206a5a72b050826fcaaa455d15a746b771d8b1c
Alex <AlexSabourinDev@gmail.com>
2020-02-11 07:11:02 -0500

    MOD: Moved platform specific code to platform source file and platform header.

--- cranberray.c
+++ cranberray.c
@@ -1,44 +1,6 @@
-#pragma warning(disable:4204) // Disable aggregate initializers warning. It's definitely valid in C.
-
 #include <stdint.h>
 
-#include <windows.h>
-#include <wingdi.h>
-
-static void cray_write_bmp(const char* fileName, uint8_t* pixels, uint32_t width, uint32_t height)
-{
-	const uint32_t stride = 4;
-
-	BITMAPFILEHEADER fileHeader = 
-	{
-		.bfType = 'MB', // BM inversed.
-		.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + width * height * stride,
-		.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) // This is bytes, no bits. Lies!
-	};
-
-	BITMAPINFOHEADER bitmapInfoHeader =
-	{
-		.biSize = sizeof(BITMAPINFOHEADER),
-		.biWidth = width,
-		.biHeight = height,
-		.biPlanes = 1,
-		.biBitCount = (WORD)stride * 8,
-		.biCompression = BI_RGB,
-		.biSizeImage = 0 // 0 for BI_RGB is fine
-	};
-
-	// Write!
-	{
-		HANDLE fileHandle = CreateFile(fileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
-
-		DWORD bytesWritten;
-		WriteFile(fileHandle, &fileHeader, sizeof(BITMAPFILEHEADER), &bytesWritten, NULL);
-		WriteFile(fileHandle, &bitmapInfoHeader, sizeof(BITMAPINFOHEADER), &bytesWritten, NULL);
-		WriteFile(fileHandle, pixels, width * height * stride, &bytesWritten, NULL);
-
-		CloseHandle(fileHandle);
-	}
-}
+#include "cranberry_platform.h"
 
 int main()
 {
@@ -47,6 +9,7 @@ int main()
 		0xFF,0x00,0x00,0x00,  0xFF,0x00,0x00,0x00,
 		0x00,0xFF,0x00,0x00,  0x00,0xFF,0x00,0x00,
 	};
-	cray_write_bmp("render.bmp", testBitmap, 2, 2);
+	cranpl_write_bmp("render.bmp", testBitmap, 2, 2);
+
 	return 0;
 }
---
+++ cranberry_win32.c
@@ -0,0 +1,107 @@
+#include "cranberry_platform.h"
+
+#define WIN32_LEAN_AND_MEAN
+#include <windows.h>
+#include <wingdi.h>
+
+void cranpl_write_bmp(const char* fileName, uint8_t* pixels, uint32_t width, uint32_t height)
+{
+	const uint32_t stride = 4;
+
+	BITMAPFILEHEADER fileHeader = 
+	{
+		.bfType = 'MB', // BM inversed.
+		.bfSize = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) + width * height * stride,
+		.bfOffBits = sizeof(BITMAPFILEHEADER) + sizeof(BITMAPINFOHEADER) // This is bytes, no bits. Lies!
+	};
+
+	BITMAPINFOHEADER bitmapInfoHeader =
+	{
+		.biSize = sizeof(BITMAPINFOHEADER),
+		.biWidth = width,
+		.biHeight = height,
+		.biPlanes = 1,
+		.biBitCount = (WORD)stride * 8,
+		.biCompression = BI_RGB,
+		.biSizeImage = 0 // 0 for BI_RGB is fine
+	};
+
+	// Write!
+	{
+		HANDLE fileHandle = CreateFile(fileName, GENERIC_WRITE, 0, NULL, CREATE_ALWAYS, FILE_ATTRIBUTE_NORMAL, NULL);
+
+		DWORD bytesWritten;
+		WriteFile(fileHandle, &fileHeader, sizeof(BITMAPFILEHEADER), &bytesWritten, NULL);
+		WriteFile(fileHandle, &bitmapInfoHeader, sizeof(BITMAPINFOHEADER), &bytesWritten, NULL);
+		WriteFile(fileHandle, pixels, width * height * stride, &bytesWritten, NULL);
+
+		CloseHandle(fileHandle);
+	}
+}
+
+static LRESULT CALLBACK WndProc(HWND hwnd, UINT msg, WPARAM wParam, LPARAM lParam)
+{
+    switch(msg)
+    {
+        case WM_DESTROY:
+            PostQuitMessage(0);
+        break;
+        default:
+            return DefWindowProc(hwnd, msg, wParam, lParam);
+    }
+    return 0;
+}
+
+void* cranpl_create_window(const char* windowName, uint32_t width, uint32_t height)
+{
+	HINSTANCE hinstance = GetModuleHandle(NULL);
+	RegisterClass(&(WNDCLASS)
+	{
+		.lpfnWndProc = WndProc,
+		.hInstance = hinstance,
+		.lpszClassName = windowName
+	});
+
+	HWND hwnd = CreateWindowEx(
+		0,
+		windowName,
+		windowName,
+		WS_OVERLAPPEDWINDOW,
+		CW_USEDEFAULT, CW_USEDEFAULT, width, height,
+		NULL,
+		NULL,
+		hinstance,
+		NULL
+		);
+	ShowWindow(hwnd, SW_SHOWNORMAL);
+
+	return hwnd;
+}
+
+bool cranpl_tick_window(void* windowHandle)
+{
+	(void)windowHandle;
+
+	bool isDone = false;
+
+	MSG msg;
+	while(PeekMessageW(&msg, NULL, 0, 0, PM_REMOVE))
+	{
+		if (msg.message == WM_QUIT)
+		{
+			isDone = true;
+		}
+		else
+		{
+			TranslateMessage(&msg);
+			DispatchMessage(&msg);
+		}
+	}
+
+	return isDone;
+}
+
+void cranpl_destroy_window(void* windowHandle)
+{
+	DestroyWindow(windowHandle);
+}
